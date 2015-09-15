e9e79a1f30e6c8fca4370611ec869eba72bbff85
Catherine <ducharme.catherine@gmail.com>
2015-09-15 20:00:45 +0000

    Created a program that tracks the latitude and longitude of the International Space Station.

---
+++ iss.js
@@ -0,0 +1,11 @@
+
+var issLocation = require('request');
+
+issLocation('http://api.open-notify.org/iss-now.json', function (error, response, body) {
+  if (!error) {
+    var issPoz = JSON.parse(body);
+    console.log("The space station is currently at latitude " + Math.round(issPoz.iss_position.latitude * 100)/100 + " and at longitude " + Math.round(issPoz.iss_position.longitude * 100)/100 + "." ); 
+  } else {
+      console.log("There has been an error of type " + error);
+  }
+})
