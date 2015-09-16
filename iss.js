0c2647c8d7236dbc23176b6395d43adb947d09b4
Catherine <ducharme.catherine@gmail.com>
2015-09-16 18:29:46 +0000

    Fixed the name of the request variable for clarity.

--- iss.js
+++ iss.js
@@ -1,7 +1,7 @@
 
-var issLocation = require('request');
+var request = require('request');
 
-issLocation('http://api.open-notify.org/iss-now.json', function (error, response, body) {
+request('http://api.open-notify.org/iss-now.json', function (error, response, body) {
   if (!error) {
     var issPoz = JSON.parse(body);
     console.log("The space station is currently at latitude " + Math.round(issPoz.iss_position.latitude * 100)/100 + " and at longitude " + Math.round(issPoz.iss_position.longitude * 100)/100 + "." ); 
